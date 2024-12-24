7d514a6c385402fb02abb6872bf702121d488dcb
daniel <dmutaka7@gmail.com>
2024-12-24 12:32:12 +0100

    TD04 : Gestion de menu dynamique

--- td_react/src/App.jsx
+++ td_react/src/App.jsx
@@ -1,75 +1,264 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { use } from 'react'
+import data from './data/data.json'
+
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
 
 
-function Notes() {
-  return (
-    <div>Notes</div>
-  )
-}
+const MENUS = [
+  { title: 'NOTE' },
+  { title: 'ETUDIANTS' },
+  { title: 'MATIERES' },
+  { title: 'RANDOM STUDENT' }
+]
 
-function Etudiants() {
-  return (
-    <div>Matieres</div>
-  )
-}
+function NOTE() {
+  const [studentData, setStudentData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
-function Matieres() {
-  return (
-    <div>
-      Matieres
-    </div>
-  )
-}
-function Apropos() {
-  return (
-    <div>
-      A propos
-    </div>
-  )
-}
+  useEffect(() => {
+    setStudentData(data);
+  }, []);
 
-function Menu() {
-  const [menus, setmenus] = useState(['Notes', 'Etudiants', 'Matieres', 'A propos']);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-
-  function onclick() {
-    alert('élément cliqué')
-  }
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
-    <div className="dropdown">
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-        Menu
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        {menus.map((menu, index) => (
-          <li key={index}>
-            <a className="dropdown-item" href="#" onClick={() => onclick(menu)}>
-              {menu}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-  )
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="student grades table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ETUDIANT</TableCell>
+              <TableCell align="center">Cours</TableCell>
+              <TableCell align="right">Note</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {studentData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item) => (
+              <TableRow
+                key={item.unique_id}
+                sx={{
+                  '&:last-child td, &:last-child th': { border: 0 },
+                  '&:hover': { backgroundColor: '#f5f5f5' },  // Survol avec une couleur de fond
+                }}
+              >
+                <TableCell>{item.student.firstname} {item.student.lastname}</TableCell>
+                <TableCell align="center">{item.course}</TableCell>
+                <TableCell align="right" className={(item.grade >= 80 ? 'text-black' : 'text-danger')}>{item.grade}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={studentData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
 }
+
+
+function ETUDIANTS() {
+  const [studentData, setStudentData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    setStudentData(data);
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="student table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Nom</TableCell>
+              <TableCell>Prénom</TableCell>
+              <TableCell>Cours</TableCell>
+              <TableCell>Note</TableCell>
+              <TableCell>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {studentData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item) => (
+                <TableRow
+                  key={item.unique_id}
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+                    '&:hover': { backgroundColor: '#f5f5f5' },
+                  }}
+                >
+                  <TableCell>{item.student.id}</TableCell>
+                  <TableCell>{item.student.lastname}</TableCell>
+                  <TableCell>{item.student.firstname}</TableCell>
+                  <TableCell>{item.course}</TableCell>
+                  <TableCell>{item.grade}</TableCell>
+                  <TableCell>{item.date}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={studentData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
+}
+
+
+function MATIERES() {
+  const [studentData, setStudentData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    setStudentData(data);
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="courses table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Cours</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {studentData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item) => (
+                <TableRow
+                  key={item.unique_id}
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+                    '&:hover': { backgroundColor: '#f5f5f5' },
+                  }}
+                >
+                  <TableCell>{item.course}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={studentData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
+}
+
+function Menu({ title, clickHandle, classname }) {
+  return (
+    <a className='d-inline p-5 text-white text-decoration-none bg-secondary menu' onClick={clickHandle}>
+      <span className={classname}>
+        {title}
+      </span>
+    </a>
+
+  );
+}
+
+
 
 function Header(props) {
+  const [selected, setSelected] = useState("");
+
+  function onClickMenu(title) {
+    setSelected(title);
+  }
+  const renderContent = () => {
+    switch (selected) {
+      case 'NOTE':
+        return <NOTE />;
+      case 'ETUDIANTS':
+        return <ETUDIANTS />;
+      case 'MATIERES':
+        return <MATIERES />;
+      case 'RANDOM STUDENT':
+        return <Students />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <header>
-      <div>
-        <img src="images.jpg" alt="" />
+      <div className='container md-5'>
+
+        <div id='examples'>
+          {MENUS.map((menu, index) => (
+            <Menu
+              key={index}
+              title={menu.title}
+              clickHandle={() => onClickMenu(menu.title)}
+              classname={selected == menu.title ? 'active' : null}
+
+            />
+          ))}
+
+        </div>
+        <img src="images.jpg" alt="React Logo" />
         <h3>Introduction à React</h3>
-        <h6>A la découverte des premières notions de React       <Menu />
-        </h6>
+        <h6>A la découverte des premières notions de React</h6>
+
+        {renderContent()}
+
       </div>
     </header>
-  )
+  );
 }
-
 function MainContent() {
   const date = new Date();
   const year = date.getFullYear();
@@ -82,9 +271,7 @@ function MainContent() {
   return (
 
     <main>
-      <p>Ici, nous afficherons des informations interessantes <br />
-        Bonjour, on est le {day}, {month}, {year} et il est {hour}:{minute}:{second}"
-      </p>
+      <button className='btn btn-secondary'>{day} /{month} / {year}</button>   <button className='btn btn-danger'>{hour}:{minute}:{second}</button>
 
     </main>
   )
@@ -203,6 +390,7 @@ function Students() {
     },
 
   ];
+
   const [nombres, setnombres] = useState(Math.floor(Math.random() * 10))
   const student = data[nombres];
   return (
@@ -268,11 +456,12 @@ function Footer() {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer className='foot'>
-      <div >
+    <footer className="fixed-bottom bg-dark text-white py-3">
+      <div className="container text-center">
         © {year} - MUTAKA DANIEL, Tous droits réservés.
       </div>
     </footer>
+
   )
 }
 
@@ -284,8 +473,7 @@ function App() {
   return (
     <>
       <div>
-        <Header name='test' />
-        <Students />
+        <Header />
         <MainContent />
         <Footer />
       </div>
